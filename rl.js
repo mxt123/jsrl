b13fc08c594296b4eebebfc4c75dd3a8f21987fd
= <=>
2015-04-28 11:28:20 +0100

    refresh screen after move

--- rl.js
+++ rl.js
@@ -7,11 +7,11 @@
 var FONT = 32;
 
 // map dimensions
-var ROWS = 10;
-var COLS = 15;
+var ROWS = 3;//10;
+var COLS = 3;//15;
 
 // number of actors per level, including player
-var ACTORS = 10;
+var ACTORS = 2;//10;
 
 // a list of all actors; 0 is the player
 var player;
@@ -58,15 +58,6 @@ function initActors() {
         livingEnemies = ACTORS-1;
 }
 
-function onKeyUp(event) {
-        // draw map to overwrite previous actors positions
-        drawMap();
- 
-        // draw actors in new positions
-        drawActors();
-}
-
-
 function initMap() {
         // create a new random map
         map = [];
@@ -82,7 +73,7 @@ function initMap() {
         }
 }
 
-function drawMap() {
+function drawMap() {    
     for (var y = 0; y < ROWS; y++)
         for (var x = 0; x < COLS; x++) {
             asciidisplay[y][x].content = map[y][x];            
@@ -92,8 +83,8 @@ function drawMap() {
 function drawActors() {       
     for (var a in actorList) {
     	if (actorList[a] != null && actorList[a].hp > 0) {
-        	var c = a == 0?''+player.hp:'e'
-            asciidisplay[actorList[a].y][actorList[a].x].content = c;                       
+        	var c = a == 0?''+player.hp:'e';      
+            asciidisplay[actorList[a].y][actorList[a].x].content = c;                      
         }
     }
 }
@@ -106,16 +97,7 @@ function create() {
     initMap();
     initActors();
 
-    // initialize ascii display
-    asciidisplay = [];
-    // initialize ascii display
-	asciidisplay = [];
-	for (var y = 0; y < ROWS; y++) {
-		var newRow = [];
-		asciidisplay.push(newRow);
-		for (var x = 0; x < COLS; x++)
-			newRow.push(initCell('', x, y));
-	}
+  	init();
 
     drawMap();
     drawActors();
@@ -123,20 +105,120 @@ function create() {
    	refresh();
 }
 
-function refresh() {
+function init() {
+  // initialize ascii display
+	asciidisplay = [];
+	for (var y = 0; y < ROWS; y++) {
+		var newRow = [];
+		asciidisplay.push(newRow);
+		for (var x = 0; x < COLS; x++)
+			newRow.push(initCell('', x, y));
+	}
+}
+
+function refresh() { 
+    
     for (var y = 0; y < ROWS; y++) {
-        for (var x = 0; x < COLS; x++)           
-        	initCell(asciidisplay[y][x].content, x, y);
+        for (var x = 0; x < COLS; x++)  {  
+            var text = asciidisplay[y][x].content;
+            asciidisplay[y][x].destroy();
+           asciidisplay[y][x] = initCell(text, x, y);
+        }
     }
+        
 }
 
 function initCell(chr, x, y) {
-    // add a single cell in a given position to the ascii display
+
+    // add a single cell in a given position to the ascii display   
     var style = {
         font: FONT + "px monospace",
         fill: "#fff"
-    };
+    };    
     return game.add.text(FONT * 0.6 * x, FONT * y, chr, style);
 }
+
+function canGo(actor,dir) {
+    return  actor.x+dir.x >= 0 &&
+        actor.x+dir.x <= COLS - 1 &&
+                actor.y+dir.y >= 0 &&
+        actor.y+dir.y <= ROWS - 1 &&
+        map[actor.y+dir.y][actor.x +dir.x] == '.';
+}
+
+function moveTo(actor, dir) {
+        // check if actor can move in the given direction
+        if (!canGo(actor,dir))
+                return false;
+ 
+        // moves actor to the new location
+        var newKey = (actor.y + dir.y) +'_' + (actor.x + dir.x);
+        // if the destination tile has an actor in it
+        if (actorMap[newKey] != null) {
+                //decrement hitpoints of the actor at the destination tile
+                var victim = actorMap[newKey];
+                victim.hp--;
+ 
+                // if it's dead remove its reference
+                if (victim.hp == 0) {
+                        actorMap[newKey]= null;
+                        actorList[actorList.indexOf(victim)]=null;
+                        if(victim!=player) {
+                                livingEnemies--;
+                                if (livingEnemies == 0) {
+                                        // victory message
+                                        var victory = game.add.text(game.world.centerX, game.world.centerY, 'Victory!\nCtrl+r to restart', { fill : '#2e2', align: "center" } );
+                                        victory.anchor.setTo(0.5,0.5);
+                                }
+                        }
+                }
+        } else {
+
+                // remove reference to the actor's old position                
+                actorMap[actor.y + '_' + actor.x]= null;            
+               // initCell("H",actor.x,actor.y);
+ 
+                // update position
+                actor.y+=dir.y;
+                actor.x+=dir.x;
+ 
+                // add reference to the actor's new position
+                actorMap[actor.y + '_' + actor.x]=actor;
+        }
+        return true;
+	}
+
+	function onKeyUp(event) {
+        // draw map to overwrite previous actors positions
+       // drawMap();
+ 
+        // act on player input
+        var acted = false;
+        switch (event.keyCode) {
+                case Phaser.Keyboard.LEFT:
+                        acted = moveTo(player, {x:-1, y:0});
+                        break;
+ 
+                case Phaser.Keyboard.RIGHT:
+                        acted = moveTo(player,{x:1, y:0});
+                        break;
+ 
+                case Phaser.Keyboard.UP:
+                        acted = moveTo(player, {x:0, y:-1});
+                        break;
+ 
+                case Phaser.Keyboard.DOWN:
+                        acted = moveTo(player, {x:0, y:1});
+                        break;
+        }
+ 
+        // draw actors in new positions
+        drawMap();
+        drawActors();        
+     //   init();
+        refresh();
+	}
+
+
 
 }());
